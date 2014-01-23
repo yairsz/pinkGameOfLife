ec86ee6184b137050baed808e081f063bb3d3393
Yair Szarf <yair@mac.com>
2014-01-23 10:45:42 -0800

    Initial Commit

---
+++ GameOfLife/YSViewController.h
@@ -0,0 +1,13 @@
+//
+//  YSViewController.h
+//  GameOfLife
+//
+//  Created by Yair Szarf on 1/23/14.
+//  Copyright (c) 2014 The 2 Handed Consortium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSViewController : UIViewController
+
+@end
---
+++ GameOfLife/YSViewController.m
@@ -0,0 +1,29 @@
+//
+//  YSViewController.m
+//  GameOfLife
+//
+//  Created by Yair Szarf on 1/23/14.
+//  Copyright (c) 2014 The 2 Handed Consortium. All rights reserved.
+//
+
+#import "YSViewController.h"
+
+@interface YSViewController ()
+
+@end
+
+@implementation YSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
